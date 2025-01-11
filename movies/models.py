82616bee8800939a59eb828003f02986a3fe871b
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-01-11 10:53:41 -0500

    examen 2

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    director_name = models.CharField(max_length=100)
+    release_year = models.DateField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return self.title
